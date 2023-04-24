71df6dfba515a9b17b367a1a2ced85133f0c0631
juhyeon-mael-lee <109581483+juhyeon-mael-lee@users.noreply.github.com>
2023-04-24 17:55:14 +0900

    feat: create character & add animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 inputVec;
+    public float speed = 3f;
+
+    private Rigidbody2D _rigid;
+    private SpriteRenderer _sprite;
+    private Animator _anim;
+
+    private void Awake()
     {
-        
+        _rigid = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        _rigid.MovePosition(_rigid.position + nextVec);
     }
-}
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        _anim.SetFloat("Speed", inputVec.magnitude);
+            
+        if (inputVec.x != 0)
+        {
+            _sprite.flipX = inputVec.x < 0;
+        }
+    }
+} 
